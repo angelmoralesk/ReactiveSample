d34704b78984895a1594bc314869191e4386523a
Angel Morales <a.jesse3001@gmail.com>
2017-12-29 21:04:34 -0600

    project setup

---
+++ ReactiveSample/ReactiveSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ReactiveSample
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 29/12/17.
+//  Copyright © 2017 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
